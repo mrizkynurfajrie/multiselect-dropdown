9ee2b89abe496a697c0b5d7ee9ace5a0cedb6592
Muhammad Rizky Nur Fajrie <86530834+mrizkynurfajrie@users.noreply.github.com>
2025-07-10 15:28:28 +0700

    add search focus node

--- lib/src/widgets/dropdown.dart
+++ lib/src/widgets/dropdown.dart
@@ -13,6 +13,7 @@ class _Dropdown<T> extends StatelessWidget {
     required this.maxSelections,
     required this.items,
     required this.onItemTap,
+    this.searchFocusNode,
     Key? key,
     this.onSearchChange,
     this.itemBuilder,
@@ -40,6 +41,8 @@ class _Dropdown<T> extends StatelessWidget {
 
   /// The decoration of the search field.
   final SearchFieldDecoration searchDecoration;
+
+  final FocusNode? searchFocusNode;
 
   /// The maximum number of selections allowed.
   final int maxSelections;
@@ -95,6 +98,7 @@ class _Dropdown<T> extends StatelessWidget {
             children: [
               if (searchEnabled)
                 _SearchField(
+                  focusNode: searchFocusNode!,
                   decoration: searchDecoration,
                   onChanged: _onSearchChange,
                 ),
@@ -197,6 +201,7 @@ class _Dropdown<T> extends StatelessWidget {
 
 class _SearchField extends StatelessWidget {
   const _SearchField({
+    required this.focusNode,
     required this.decoration,
     required this.onChanged,
   });
@@ -205,11 +210,14 @@ class _SearchField extends StatelessWidget {
 
   final ValueChanged<String> onChanged;
 
+  final FocusNode focusNode;
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8),
       child: TextField(
+        focusNode: focusNode,
         decoration: InputDecoration(
           isDense: true,
           hintText: decoration.hintText,
